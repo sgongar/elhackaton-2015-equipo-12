b675f04949f5abe71c521fb835d31514707cec15
Samuel Gongora Garcia <s.gongoragarcia@gmail.com>
2015-10-04 09:08:11 +0200

    Initial commit.

---
+++ sketch_emisor/sketch_emisor.ino
@@ -0,0 +1,65 @@
+//#include <SoftwareSerial.h>
+
+#define RX_PIN 0
+#define TX_PIN 1
+
+// SoftwareSerial BT(RX_PIN, TX_PIN);
+
+int interruptor = 2;
+int ledPin = 3;
+char texto;
+int flag;
+int state;
+int flag_loop = 1;
+char flag_state;
+
+void setup() 
+{
+  pinMode(interruptor, INPUT);
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+}
+ 
+void loop() 
+{
+    int valor_interruptor = digitalRead(interruptor);
+   
+    if(Serial.available() > 0)
+    {
+      state = Serial.read();
+      flag_state = state;
+      flag = 0;
+    }
+
+    
+    if (valor_interruptor == 1)
+    {
+      delay(50);
+      flag = 0;
+      flag_state = 'p';
+    }
+    // if the state is 0 the led will turn off
+    /*
+     * Envia un ping al receptor
+     */
+    if (flag_state == 'p') 
+    {
+      if (flag == 0)
+      {
+        Serial.println("hola receptor");
+        digitalWrite(ledPin, HIGH);
+        flag_state = 0;
+      }
+    }
+      
+    else if (flag_state == 'r') 
+    {
+      digitalWrite(ledPin, LOW);
+      if(flag == 0)
+      {
+        Serial.println("LED: off");
+        flag = 1;
+      }
+    } 
+  }
+
---
+++ sketch_receptor/sketch_receptor.ino
@@ -0,0 +1,80 @@
+//#include <SoftwareSerial.h>
+
+
+
+#define RX_PIN 0
+#define TX_PIN 1
+
+// SoftwareSerial BT(RX_PIN, TX_PIN);
+
+int interruptor = 2;
+int ledPin = 7;
+int led;
+char texto;
+int flag;
+int state;
+int flag_loop = 1;
+char flag_state;
+
+void setup() 
+{
+  pinMode(interruptor, INPUT);
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+}
+ 
+void loop() 
+{
+    int valor_interruptor = digitalRead(interruptor);
+    if(Serial.available() > 0)
+    {
+      state = Serial.read();
+      flag_state = state;
+      flag = 0;
+    }
+    
+    if (valor_interruptor == 1)
+    {
+      flag = 1;
+      flag_state = 'r';
+    }
+    // if the state is 0 the led will turn off
+    
+    if (flag_state == 'p') 
+    {
+      if (flag == 0)
+      {
+        int i;
+        digitalWrite(ledPin, HIGH);
+        tone(12, 261, 1000);
+        delay(1000);
+        tone(12, 329, 1000);
+        delay(1250);
+        tone(12, 391, 1000);
+        delay(1750);
+        tone(12, 523, 1000);
+      }
+
+      if(flag == 0)
+      {
+        // Encender sonido y luces
+        // Serial.println("");
+        flag = 1;
+      }
+    }
+      
+    else if (flag_state == 'r') 
+    {
+      digitalWrite(ledPin, LOW);
+      if(flag == 0)
+      {
+        Serial.println("LED: off");
+        flag = 1;
+      }
+    } 
+  }
+
+
+
+
+
